ef0a3bbdd0f68e044378f36a0c3fda613fdecdb0
Daily-manjoo <yuslz35@gmail.com>
2023-06-15 01:43:33 +0900

    0615 commit

--- index.js
+++ index.js
@@ -8,3 +8,24 @@ var slides = document.querySelector(".new-item-list"),
   nextBtn = document.querySelector("#next");
 
 slides.style.width = (slideWidth + slideMargin) * slideCount - slideMargin + "px";
+
+function moveSlide(num) {
+  slides.style.left = -num * 330 + "px";
+  currentIdx = num;
+}
+
+nextBtn.addEventListener("click", function () {
+  if (currentIdx < slideCount - 3) {
+    moveSlide(currentIdx + 1);
+  } else {
+    moveSlide(0);
+  }
+});
+
+prevBtn.addEventListener("click", function () {
+  if (currentIdx > 0) {
+    moveSlide(currentIdx - 1);
+  } else {
+    moveSlide(currentIdx < slideCount - 3);
+  }
+});
